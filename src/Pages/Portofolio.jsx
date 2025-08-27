3f764c83b24f5ef7bee4fc9af09554e021107615
hamas22 <168597445+hamas22@users.noreply.github.com>
2025-08-27 21:50:51 +0300

    vite

--- src/Pages/Portofolio.jsx
+++ src/Pages/Portofolio.jsx
@@ -104,7 +104,6 @@ const techStacks = [
   { icon: "javascript.svg", language: "JavaScript" },
   { icon: "tailwind.svg", language: "Tailwind CSS" },
   { icon: "reactjs.svg", language: "ReactJS" },
-  { icon: "vite.svg", language: "Vite" },
   { icon: "bootstrap.svg", language: "Bootstrap" },
   { icon: "firebase.svg", language: "Firebase" },  
 ];
