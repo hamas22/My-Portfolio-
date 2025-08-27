124e0961d317c79c9fc6d6463380d95533a01d71
hamas22 <168597445+hamas22@users.noreply.github.com>
2025-08-27 21:09:44 +0300

    resume

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -231,7 +231,7 @@ const AboutPage = () => {
       </div>
 
            <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-  <a href="../../dist\Resume Hamas.pdf" download className="w-full lg:w-auto">
+  <a href="../public/resume.pdf" download className="w-full lg:w-auto">
     <button 
       data-aos="fade-up"
       data-aos-duration="800"
